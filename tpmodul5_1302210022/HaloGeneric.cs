49662d2b9e4eee5f221d6e056bc4502f733946ce
ibrahimmt11 <abitmulyo@gmail.com>
2023-03-19 18:39:21 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_1302210022/HaloGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302210022
+{
+    internal class HaloGeneric
+    {
+        public void SapaUser<X>(X user)
+        {
+            Console.WriteLine("Halo user " + user);
+        }
+    }
+}
--- tpmodul5_1302210022/Program.cs
+++ tpmodul5_1302210022/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul5_1302210022;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        string nama = "Ibrahim";
+        halo.SapaUser(nama);
+    }
+}
